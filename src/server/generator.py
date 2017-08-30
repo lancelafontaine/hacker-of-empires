321a71e89bcdb3c9cfb0245986fdcab70d9c9c94
Lance Lafontaine <lance.lafontaine92@gmail.com>
2017-08-30 19:11:29 -0400

    Create sqlite database table for user information

--- src/server/generator.py
+++ src/server/generator.py
@@ -2,6 +2,7 @@ from random import SystemRandom
 import data
 from flask import Flask, jsonify, request
 from flask_cors import CORS
+import sqlite3
 
 allowed_hosts = [
     'http://localhost:8080',
@@ -20,6 +21,9 @@ def generator():
         program = data.get("major", "soen").strip().lower()
         language = data.get("language", "java").strip().lower()
         colour = data.get("colour", "blue").strip().lower()
+        email = data.get("email", "fake@fake.fake").strip().lower()
+
+        save_to_db(program, language, colour, email)
 
         return jsonify(text=generate(program, language, colour))
 
@@ -55,6 +59,16 @@ def generate(program, language, colour):
     }
 
     return response_data
+
+def save_to_db(program, language, colour, email):
+    connection = sqlite3.connect('example.db')
+    c = connection.cursor()
+
+    c.execute('CREATE TABLE IF NOT EXISTS hackers (program text, language text, colour text, email text)')
+    c.execute('INSERT INTO hackers VALUES (?,?,?,?)', (program, language, colour, email))
+
+    connection.commit()
+    connection.close()
 
 if __name__ == "__main__":
     app.run()
