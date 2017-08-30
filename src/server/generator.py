1faf08a39f09819bb53541b1e5ce618266a700b5
Arielle Evans <ariellelevans@gmail.com>
2017-08-30 14:01:42 -0400

    fixed defaults and cleaned up string formatting a bit

--- src/server/generator.py
+++ src/server/generator.py
@@ -4,50 +4,42 @@ from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
+
 @app.route('/', methods=["POST", "OPTIONS"])
 def generator():
-    if request.method == "OPTIONS":
+    if request.method != "POST":
         return jsonify({})
-    if request.method == "POST":
-        data = request.get_json()
-        program = data.get("major", "Software Engineering")
-        language = data.get("language", "Java")
-        colour = data.get("colour", "Blue")
 
-        return jsonify(text=generate(program, language, colour))
+    data = request.get_json()
+    program = data.get("major", "soen")
+    language = data.get("language", "java")
+    colour = data.get("colour", "blue")
+
+    return jsonify(text=generate(program, language, colour))
 
 def generate(program, language, colour):
     rand = SystemRandom()
 
-    class_name = rand.choice(data.classes[program])
-    first_name = rand.choice(data.first_names)
-    title = rand.choice(data.titles)
+    skill_nums = [str(i) if i < 0 else "+{}".format(i) for i in range(-10, 11)]
+    skill_nums.remove("+0")
 
-    misc_skill = rand.choice(data.skills["Misc"])
-    language_skill = rand.choice(data.skills[language])
-    colour_skill = rand.choice(data.skills[colour])
-    skill_nums = [rand.randint(-10, 10) for i in range(3)]
-    skill_nums = [i if i < 0 else "+{}".format(i) for i in skill_nums]
+    skills = [
+        "{} {}".format(rand.choice(skill_nums), rand.choice(data.skills["misc"])),
+        "{} {}".format(rand.choice(skill_nums), rand.choice(data.skills[language])),
+        "{} {}".format(rand.choice(skill_nums), rand.choice(data.skills[colour]))
+    ]
+
+    name = "{} {}, {}".format(rand.choice(data.first_names), rand.choice(data.titles), rand.choice(data.classes[program]))
 
     final = """
-    {} {}, {}
+    {}
 
-    {} {}
-    {} {}
-    {} {}
-    """.format(first_name, title, class_name,
-               skill_nums[0], misc_skill,
-               skill_nums[1], language_skill,
-               skill_nums[2], colour_skill)
-
-    print(final)
-    print(final)
-    print(final)
-    print(final)
+    {}
+    {}
+    {}
+    """.format(name, *skills)
 
     return final
 
-
 if __name__ == "__main__":
-    character = generate("Computer Engineering", "Java", "Blue")
     app.run()
